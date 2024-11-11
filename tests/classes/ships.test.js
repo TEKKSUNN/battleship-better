8e371c671bb000cac127b9b8a166aa4d833b7c76
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-11 17:40:04 +0800

    tests: Make tests for class Ship

--- tests/classes/ships.test.js
+++ tests/classes/ships.test.js
@@ -9,38 +9,50 @@ describe("Ship Class", () => {
           t: {
             length: 5,
             hits: 0,
-            misses: 0
+            misses: 0,
           },
           s: new Ship(3),
           t: {
             length: 3,
             hits: 0,
-            misses: 0
+            misses: 0,
           },
           s: new Ship(1),
           t: {
             length: 1,
             hits: 0,
-            misses: 0
+            misses: 0,
           },
-        }
+        },
       ];
-      ships.forEach(ship => {
+      ships.forEach((ship) => {
         expect(ship.s).toEqual(ship.t);
       });
     });
 
     test("should throw an error when length has decimal", () => {
-      expect(() => new Ship(5.10)).toThrow("Ship Error: Ship must have an integer as length.");
-      expect(() => new Ship(2.10)).toThrow("Ship Error: Ship must have an integer as length.");
-      expect(() => new Ship(3.99)).toThrow("Ship Error: Ship must have an integer as length.");
-    })
+      expect(() => new Ship(5.1)).toThrow(
+        "Ship Error: Ship must have an integer as length.",
+      );
+      expect(() => new Ship(2.1)).toThrow(
+        "Ship Error: Ship must have an integer as length.",
+      );
+      expect(() => new Ship(3.99)).toThrow(
+        "Ship Error: Ship must have an integer as length.",
+      );
+    });
 
     test("should throw an error when length is less than or equal to 0", () => {
-      expect(() => new Ship(0)).toThrow("Ship Error: Ship length cannot be less than or equal to 0.");
-      expect(() => new Ship(-10)).toThrow("Ship Error: Ship length cannot be less than or equal to 0.");
-      expect(() => new Ship(-218)).toThrow("Ship Error: Ship length cannot be less than or equal to 0.");
-    })
+      expect(() => new Ship(0)).toThrow(
+        "Ship Error: Ship length cannot be less than or equal to 0.",
+      );
+      expect(() => new Ship(-10)).toThrow(
+        "Ship Error: Ship length cannot be less than or equal to 0.",
+      );
+      expect(() => new Ship(-218)).toThrow(
+        "Ship Error: Ship length cannot be less than or equal to 0.",
+      );
+    });
   });
 
   describe("Methods", () => {
@@ -48,16 +60,12 @@ describe("Ship Class", () => {
     let ships;
 
     beforeEach(() => {
-      ships = [
-        new Ship(5),
-        new Ship(4),
-        new Ship(3)
-      ]
+      ships = [new Ship(5), new Ship(4), new Ship(3)];
     });
 
     describe("hit()", () => {
       test("should increment the ship's hits", () => {
-        ships.forEach(ship => {
+        ships.forEach((ship) => {
           const hitsBefore = ship.hits;
           ship.hit();
           ship.hit();
@@ -68,7 +76,7 @@ describe("Ship Class", () => {
 
     describe("isSunk()", () => {
       test("should return true when ship is sunk", () => {
-        ships.forEach(ship => {
+        ships.forEach((ship) => {
           for (let i = 0; i < ship.length; i++) {
             ship.hit();
           }
@@ -77,7 +85,7 @@ describe("Ship Class", () => {
       });
 
       test("should return false when ship is not sunk", () => {
-        ships.forEach(ship => {
+        ships.forEach((ship) => {
           for (let i = 0; i < ship.length - 1; i++) {
             ship.hit();
           }
