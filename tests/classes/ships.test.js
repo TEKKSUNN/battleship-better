6bc94c52ea8b68bc1d2b62170b121e6638289922
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-11 17:30:34 +0800

    build: Fix "fix" linting everything

---
+++ tests/classes/ships.test.js
@@ -0,0 +1,89 @@
+import Ship from "../../src/classes/ships";
+
+describe("Ship Class", () => {
+  describe("Constructor", () => {
+    test("should return a ship object", () => {
+      const ships = [
+        {
+          s: new Ship(5),
+          t: {
+            length: 5,
+            hits: 0,
+            misses: 0
+          },
+          s: new Ship(3),
+          t: {
+            length: 3,
+            hits: 0,
+            misses: 0
+          },
+          s: new Ship(1),
+          t: {
+            length: 1,
+            hits: 0,
+            misses: 0
+          },
+        }
+      ];
+      ships.forEach(ship => {
+        expect(ship.s).toEqual(ship.t);
+      });
+    });
+
+    test("should throw an error when length has decimal", () => {
+      expect(() => new Ship(5.10)).toThrow("Ship Error: Ship must have an integer as length.");
+      expect(() => new Ship(2.10)).toThrow("Ship Error: Ship must have an integer as length.");
+      expect(() => new Ship(3.99)).toThrow("Ship Error: Ship must have an integer as length.");
+    })
+
+    test("should throw an error when length is less than or equal to 0", () => {
+      expect(() => new Ship(0)).toThrow("Ship Error: Ship length cannot be less than or equal to 0.");
+      expect(() => new Ship(-10)).toThrow("Ship Error: Ship length cannot be less than or equal to 0.");
+      expect(() => new Ship(-218)).toThrow("Ship Error: Ship length cannot be less than or equal to 0.");
+    })
+  });
+
+  describe("Methods", () => {
+    // Make a test value that will be initialized each test.
+    let ships;
+
+    beforeEach(() => {
+      ships = [
+        new Ship(5),
+        new Ship(4),
+        new Ship(3)
+      ]
+    });
+
+    describe("hit()", () => {
+      test("should increment the ship's hits", () => {
+        ships.forEach(ship => {
+          const hitsBefore = ship.hits;
+          ship.hit();
+          ship.hit();
+          expect(ship.hits).toBe(hitsBefore + 2);
+        });
+      });
+    });
+
+    describe("isSunk()", () => {
+      test("should return true when ship is sunk", () => {
+        ships.forEach(ship => {
+          for (let i = 0; i < ship.length; i++) {
+            ship.hit();
+          }
+          expect(ship.isSunk()).toBeTruthy();
+        });
+      });
+
+      test("should return false when ship is not sunk", () => {
+        ships.forEach(ship => {
+          for (let i = 0; i < ship.length - 1; i++) {
+            ship.hit();
+          }
+          expect(ship.isSunk()).toBeFalsy();
+        });
+      });
+    });
+  });
+});
