337307a55f2f717c224a51c805765be811d68ef3
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-13 15:12:17 +0800

    feat: Make gameboard templates "Player" & "Computer"

---
+++ src/classes/players.js
@@ -0,0 +1,13 @@
+import Gameboard from "./gameboard";
+
+export class Player extends Gameboard {
+  constructor() {
+    super();
+  }
+}
+
+export class Computer extends Gameboard {
+  constructor() {
+    super();
+  }
+}
