0839c31f7aba43d8d6bc4847fc8c81d8ac28bec5
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-15 13:05:23 +0800

    tests: Expect error if no imageURL createImageButton

--- tests/dom/elements/buttons.test.js
+++ tests/dom/elements/buttons.test.js
@@ -1,10 +1,21 @@
-import { createButton } from "../../../src/dom/elements/buttons";
+import {
+  createButton,
+  createImageButton,
+} from "../../../src/dom/elements/buttons";
 
 describe("Buttons", () => {
   describe("createButton()", () => {
     test("should throw an error when no textContent is given.", () => {
       expect(() => createButton()).toThrow(
         "createButton() must have a textContent.",
+      );
+    });
+  });
+
+  describe("createImageButton()", () => {
+    test("should throw an error when no imageURL is given.", () => {
+      expect(() => createImageButton()).toThrow(
+        "createImageButton() must have an imageURL.",
       );
     });
   });
