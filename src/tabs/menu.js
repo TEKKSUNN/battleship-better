5bd2322397c721b3c0eb5ba3c78efde4aeee4ca7
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-15 19:07:25 +0800

    feat: Make menu tab loadMenu fn

---
+++ src/tabs/menu.js
@@ -0,0 +1,31 @@
+// Get website logo.
+import WebsiteLogo from "../../assets/menu/battleship-logo.png";
+
+import { createDiv } from "../dom/elements/divs";
+import { createButton } from "../dom/elements/buttons";
+import { createImage } from "../dom/elements/images";
+import { appendAll, getContent } from "../dom/helpers";
+import loadCredits from "./credits";
+import loadSettings from "./settings";
+import loadChooseGamemode from "./menu/choose-gamemode";
+
+// Loads menu.
+export default function loadMenu() {
+  const content = getContent();
+
+  // Make the image.
+  const websiteLogo = createImage(WebsiteLogo, "web-logo", "web-logo");
+
+  // Make the buttons.
+  const buttonDiv = createDiv("btn-div");
+  const playButton = createButton("Play Game", "menu-btn", loadChooseGamemode);
+  const settingsButton = createButton("Settings", "menu-btn", loadSettings);
+  const creditsButton = createButton("Credits", "menu-btn", loadCredits);
+  appendAll(buttonDiv, playButton, settingsButton, creditsButton);
+
+  // Append all to content.
+  appendAll(content, websiteLogo, buttonDiv);
+
+  // Change class name of content.
+  content.className = "menu";
+}
