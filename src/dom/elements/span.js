a98e64ec8bfcdd75b1c6b2beb3c13870bd04f652
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-15 11:58:38 +0800

    feat: Add textContent to createSpan fn

--- src/dom/elements/span.js
+++ src/dom/elements/span.js
@@ -1,10 +1,17 @@
+import { HTMLError } from "../error";
 import create from "../helpers";
 
 // Returns an HTMLSpanElement.
-export function createSpan(className, id) {
+export function createSpan(textContent, className, id) {
   const span = create("span");
 
+  // Throw error when no textContent.
+  if (!textContent) {
+    throw new HTMLError("createSpan() must have a textContent.");
+  }
+
   // Set the attributes of span.
+  span.textContent = textContent;
   if (className) {
     span.className = className;
   }
