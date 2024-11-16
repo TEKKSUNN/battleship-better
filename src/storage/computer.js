7af58f063ca14c4e78afe4588b5b541a40d6e611
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-16 13:55:22 +0800

    feat: Make resetComputerBoard & getComputerBoard fns

---
+++ src/storage/computer.js
@@ -0,0 +1,8 @@
+import { storageGet, storageSet } from "./storage";
+import { Computer } from "../classes/players";
+
+const key = "computerBoard";
+
+export const resetComputerBoard = () => storageSet(key, new Computer());
+
+export const getComputerBoard = () => storageGet(key);
