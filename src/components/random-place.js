8d03035779e2103481452e8bfdee6f737a359701
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-16 18:51:09 +0800

    feat: Make createRandomPlaceButton fn

---
+++ src/components/random-place.js
@@ -0,0 +1,14 @@
+// Get random place button icon.
+import RandomIcon from "../../assets/place-stage/cg-random.png";
+import { getBoard } from "../dom/board/getBoard";
+
+import { createImageButton } from "../dom/elements/buttons";
+import { getPlayerBoard, setPlayerBoard } from "../storage/player";
+
+export function createRandomPlaceButton(GameboardObject, boardNode) {
+  const button = createImageButton(RandomIcon, "random-btn", () => {
+    GameboardObject.randomPlaceAllShips();
+    setPlayerBoard(GameboardObject);
+    boardNode.innerHTML = getBoard(getPlayerBoard()).innerHTML;
+  });
+}
