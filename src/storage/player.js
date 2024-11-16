b324ecdf90555ce65476c49d57d652e1fa95cb35
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-16 18:45:34 +0800

    feat: Make setPlayerBoard storage fn

--- src/storage/player.js
+++ src/storage/player.js
@@ -6,3 +6,5 @@ const key = "playerBoard";
 export const resetPlayerBoard = () => storageSet(key, new Player());
 
 export const getPlayerBoard = () => storageGet(key);
+
+export const setPlayerBoard = (newBoard) => storageSet(key, newBoard);
