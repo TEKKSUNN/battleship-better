7e2c88f765d2164e3f0806ccf12979e1295fde4d
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-16 13:57:18 +0800

    feat: Make resetPlayer2Board & getPlayer2Board fns

---
+++ src/storage/player2.js
@@ -0,0 +1,8 @@
+import { storageGet, storageSet } from "./storage";
+import { Player } from "../classes/players";
+
+const key = "player2Board";
+
+export const resetPlayer2Board = () => storageSet(key, new Player());
+
+export const getPlayer2Board = () => storageGet(key);
