0dd60e6e6f86435656798ce181d53005cbb93315
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-16 13:53:47 +0800

    feat: Make resetPlayerBoard & getPlayerBoard fn

---
+++ src/storage/player.js
@@ -0,0 +1,8 @@
+import { storageGet, storageSet } from "./storage";
+import { Player } from "../classes/players";
+
+const key = "playerBoard";
+
+export const resetPlayerBoard = () => storageSet(key, new Player());
+
+export const getPlayerBoard = () => storageGet(key);
