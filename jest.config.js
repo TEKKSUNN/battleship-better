0a4ffa881c756601e55ec6abcc48ba4c629599a2
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-16 10:08:49 +0800

    fix: Ignore images in testing

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFiles: ['<rootDir>/jest.setup.js'],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
+  moduleNameMapper: {
+    // Match static file extensions like .css, .jpg, etc.
+    '\\.(css|less|scss|sass|jpg|jpeg|png|gif|webp|svg|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+  },
 };
