3be41969d5e503a12a75a3da32b6b84430f0c2e8
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-16 19:39:19 +0800

    feat: Make storageGet also work w/ classes

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -2,6 +2,16 @@ export function storageSet(key, value) {
   sessionStorage.setItem(key, JSON.stringify(value));
 }
 
-export function storageGet(key) {
-  return JSON.parse(sessionStorage.getItem(key));
+export function storageGet(key, constructorFn) {
+  const data = JSON.parse(sessionStorage.getItem(key));
+  if (!data) return null;
+
+  if (constructorFn) {
+    // Recreate the class instance
+    const instance = new constructorFn();
+    Object.assign(instance, data); // Copy data properties to the new instance
+    return instance;
+  }
+  
+  return data;
 }
