e643e6a6bcf52f2bc9150e978e61498a06a9c6bd
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-16 14:08:23 +0800

    feat: Set instructions for loadPVE fn

--- src/tabs/playPVE.js
+++ src/tabs/playPVE.js
@@ -1,1 +1,10 @@
-export default function loadPVE() {}
+import { resetComputerBoard } from "../storage/computer";
+import { resetPlayerBoard } from "../storage/player";
+import { loadPlayerPlaceStage } from "./pve/place-stage/player";
+
+// Moves to (player) place ship phase of the game (Player Vs Computer Option).
+export default function loadPVE() {
+  resetPlayerBoard();
+  resetComputerBoard();
+  loadPlayerPlaceStage();
+}
