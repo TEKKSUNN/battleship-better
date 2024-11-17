deefc5d875c6302c8cb60b6524acd5d96837989f
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-17 14:47:49 +0800

    feat: Add name key-values for all ship templates

--- src/classes/ships.js
+++ src/classes/ships.js
@@ -24,29 +24,34 @@ export default class Ship {
 export class Battleship extends Ship {
   constructor() {
     super(4);
+    this.name = "Battleship";
   }
 }
 
 export class Cruiser extends Ship {
   constructor() {
     super(5);
+    this.name = "Cruiser";
   }
 }
 
 export class PatrolBoat extends Ship {
   constructor() {
     super(3);
+    this.name = "PatrolBoat";
   }
 }
 
 export class RescueShip extends Ship {
   constructor() {
     super(2);
+    this.name = "RescueShip";
   }
 }
 
 export class Destroyer extends Ship {
   constructor() {
     super(3);
+    this.name = "Destroyer";
   }
 }
