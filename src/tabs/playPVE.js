8a3b65b34230476e5a5aecb9ef275ead774756c6
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-17 15:54:45 +0800

    feat: Use resetAll fn on loadPVE fn

--- src/tabs/playPVE.js
+++ src/tabs/playPVE.js
@@ -1,10 +1,8 @@
-import { resetComputerBoard } from "../storage/computer";
-import { resetPlayerBoard } from "../storage/player";
+import { resetAll } from "../storage/reset";
 import { loadPlayerPlaceStage } from "./pve/place-stage/player";
 
 // Moves to (player) place ship phase of the game (Player Vs Computer Option).
 export default function loadPVE() {
-  resetPlayerBoard();
-  resetComputerBoard();
+  resetAll();
   loadPlayerPlaceStage();
 }
