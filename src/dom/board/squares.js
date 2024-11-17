edd18c480feb1e4a1146f9e2aa6de46f3bfd2e58
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-17 15:26:36 +0800

    feat: Make getSquaresWidth & getSquaresHeight fn

--- src/dom/board/squares.js
+++ src/dom/board/squares.js
@@ -1,3 +1,7 @@
-export function getSquaresSize() {
+export function getSquaresWidth() {
   return document.querySelector(".square").clientWidth;
+}
+
+export function getSquaresHeight() {
+  return document.querySelector(".square").clientHeight;
 }
