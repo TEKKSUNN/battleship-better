3812ec93582aa63eab0c1b3310e53cba7f4dd00e
TEKKSUNN <emmanuelleutecson@gmail.com>
2024-11-23 02:35:21 +0800

    tests: Expect error when no GameboardStorageFN or not fn

--- tests/dom/board/getBoard.test.js
+++ tests/dom/board/getBoard.test.js
@@ -1,4 +1,5 @@
 import { getBoard } from "../../../src/dom/board/getBoard";
+import Gameboard from "../../../src/classes/gameboard";
 
 // Test getBoard function.
 describe("getBoard()", () => {
@@ -12,5 +13,12 @@ describe("getBoard()", () => {
     const errorMsg = "getBoard() must have an object as gameboard.";
     expect(() => getBoard(1)).toThrow(errorMsg);
     expect(() => getBoard("hee hee")).toThrow(errorMsg);
+  });
+
+  test("should throw an error when no GameboardStorageFN is given or not a function.", () => {
+    const errorMsg = "getBoard() GameboardStorageFN missing or not a function";
+    expect(() => getBoard(new Gameboard())).toThrow(errorMsg);
+    expect(() => getBoard(new Gameboard(), "String")).toThrow(errorMsg);
+    expect(() => getBoard(new Gameboard(), 1)).toThrow(errorMsg);
   });
 });
