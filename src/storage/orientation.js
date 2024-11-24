e467a7032bfdcbc9e0d9cbe695beaaff7795cbc6
TEKKSUNN <emmanuelleutecson@gmail.com>
2024-11-24 15:49:00 +0800

    fix: Use lowercase values for orientation

--- src/storage/orientation.js
+++ src/storage/orientation.js
@@ -4,11 +4,11 @@ const key = "orientation";
 
 // Toggles value of orientation in sessionStorage.
 export function toggleOrientation() {
-  storageSet(key, storageGet(key) === "HORIZONTAL" ? "VERTICAL" : "HORIZONTAL");
+  storageSet(key, storageGet(key) === "horizontal" ? "vertical" : "horizontal");
 }
 
 // Gets value of orientation
 export const getOrientation = () => storageGet(key);
 
-// Resets orientation value to "HORIZONTAL"
-export const resetOrientation = () => storageSet(key, "HORIZONTAL");
+// Resets orientation value to "horizontal"
+export const resetOrientation = () => storageSet(key, "horizontal");
